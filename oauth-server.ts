15604fc0e7b7f8b14a09156e824e36dd243a04d8
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-25 20:16:20 -0600

    Basic framework.

--- oauth-server.ts
+++ oauth-server.ts
@@ -4,3 +4,29 @@ import oauth2orize from 'oauth2orize';
 import passport from 'passport';
 
 const app = express();
+
+app.use(bodyParser.urlencoded(
+    {
+        extended: true
+    }
+));
+
+const server = oauth2orize.createServer();
+
+app.use(passport.initialize());
+
+app.get('/authorize',
+    (req, res) => {
+
+    }
+);
+
+app.post('/token',
+    (req, res) => {
+
+    }
+);
+
+app.listen(3000, () => {
+    console.log("OAuth server is running on port 3000.")
+});
